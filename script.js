93a1a6a335ec77797d0fc0d70b569531d0cfe007
Sameer Sagar <60567209+sameer2002@users.noreply.github.com>
2023-09-02 20:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const output=document.getElementById('output');
+
+function createPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000);
+    });
+}
+
+createPromise().then((data)=>{
+	output.textContent = data;
+})
